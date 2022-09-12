0d6ba44204b233dae24d92751331756c9eb43e42
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2022-09-12 15:17:57 +0700

    add code

--- src/main/java/com/AssignmentApplication.java
+++ src/main/java/com/AssignmentApplication.java
@@ -1,10 +1,18 @@
 package com;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 public class AssignmentApplication {
@@ -15,8 +23,16 @@ public class AssignmentApplication {
 				.setMatchingStrategy(MatchingStrategies.LOOSE);
 		return modelMapper;
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(AssignmentApplication.class, args);
+      ClassPathResource serviceAccount = new ClassPathResource("clothesshop-1e4f2-firebase-adminsdk-go941-099ddf72d7.json");
+
+      FirebaseOptions options = new FirebaseOptions.Builder()
+              .setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+              .setStorageBucket("clothesshop-1e4f2.appspot.com")
+              .build();
+
+      FirebaseApp.initializeApp(options);
 	}
 
 }
