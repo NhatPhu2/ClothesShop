630f32bc73efa2f87a77e8e8df058a9b0e47d5d1
HiCode <74962312+hieuhoang25@users.noreply.github.com>
2022-09-21 18:23:42 +0700

    update delete file

--- src/main/java/com/controller/FileController.java
+++ src/main/java/com/controller/FileController.java
@@ -1,33 +1,40 @@
 package com.controller;
 
-
-
 import com.service.FileService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1")
 public class FileController {
-		@Autowired
-	    private FileService fileService;
-	    @PostMapping("/upload")
-	    public ResponseEntity<Object>  upload(@RequestParam("file") MultipartFile multipartFile) {
-	        return ResponseEntity.ok(fileService.upload(multipartFile));
-	    }
+	@Autowired
+	private FileService fileService;
+
+	@PostMapping("/upload")
+	public ResponseEntity<Object> upload(@RequestParam("file") MultipartFile multipartFile) {
+		return ResponseEntity.ok(fileService.upload(multipartFile));
+	}
 
 //		@PostMapping("/googleDrive/upload")
 //	public ResponseEntity<> uploadGoogle(@RequestPart("file") MultipartFile multipartFile){
 //
 //		}
 
-
-
+	@DeleteMapping("files/{url}")
+	public ResponseEntity<?> delete(@PathVariable("url") String url) {
+		try {
+			if (fileService.delete(url) == true)
+				return ResponseEntity.ok("Delete successfully!");
+			else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Delete failed!");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Delete failed!");
+		}
+		
+	}
 
 }
